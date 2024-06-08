e3587820623005c2e6bd30ae8bda2e714d795066
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-06-08 16:47:26 +0300

    add 'security' to pom

--- src/main/java/ua/tc/marketplace/config/SecurityConfig.java
+++ src/main/java/ua/tc/marketplace/config/SecurityConfig.java
@@ -3,12 +3,27 @@ package ua.tc.marketplace.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
+        .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(config -> config.requestMatchers("/api/v1/**").permitAll());
+    return http.build();
+  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
