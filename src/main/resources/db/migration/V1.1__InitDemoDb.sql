be7dcc7b769b8715bd53bf21479ca61e38d7b007
Yevhen Melnyk <yevhen.melnik@student.itmont.de>
2024-06-14 13:22:39 +0300

    fixing

---
+++ src/main/resources/db/migration/V1.1__InitDemoDb.sql
@@ -0,0 +1,21 @@
+create table demo (
+    id SERIAL PRIMARY KEY,
+    name varchar(255) not null
+);
+
+INSERT INTO demo(name) VALUES('Demo Name 1');
+INSERT INTO demo(name) VALUES('Demo Name 2');
+INSERT INTO demo(name) VALUES('Demo Name 3');
+INSERT INTO demo(name) VALUES('Demo Name 4');
+INSERT INTO demo(name) VALUES('Demo Name 5');
+INSERT INTO demo(name) VALUES('Demo Name 6');
+INSERT INTO demo(name) VALUES('Demo Name 7');
+INSERT INTO demo(name) VALUES('Demo Name 8');
+INSERT INTO demo(name) VALUES('Demo Name 9');
+INSERT INTO demo(name) VALUES('Demo Name 10');
+INSERT INTO demo(name) VALUES('Demo Name 11');
+INSERT INTO demo(name) VALUES('Demo Name 12');
+INSERT INTO demo(name) VALUES('Demo Name 13');
+INSERT INTO demo(name) VALUES('Demo Name 14');
+INSERT INTO demo(name) VALUES('Demo Name 15');
+INSERT INTO demo(name) VALUES('Demo Name 16');
