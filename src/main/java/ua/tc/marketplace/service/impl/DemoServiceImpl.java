875a3841665a30afe09de849c44e787f201f6dc9
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-06-16 10:13:26 +0300

    mapstruct integrated

--- src/main/java/ua/tc/marketplace/service/impl/DemoServiceImpl.java
+++ src/main/java/ua/tc/marketplace/service/impl/DemoServiceImpl.java
@@ -5,23 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ua.tc.marketplace.model.dto.DemoRequest;
-import ua.tc.marketplace.model.entity.Demo;
 import ua.tc.marketplace.repository.DemoRepository;
 import ua.tc.marketplace.service.DemoService;
+import ua.tc.marketplace.util.mapper.DemoMapper;
 
 @RequiredArgsConstructor
 @Service
 public class DemoServiceImpl implements DemoService {
 
   private final DemoRepository demoRepository;
+  private final DemoMapper demoMapper;
 
   @Override
   public Page<DemoRequest> findAll(Pageable pageable) {
 
-    return demoRepository.findAll(pageable).map(this::toDto);
-  }
-
-  private DemoRequest toDto(Demo demo) {
-    return new DemoRequest(demo.getName());
+    return demoRepository.findAll(pageable).map(demoMapper::toDto);
   }
 }
