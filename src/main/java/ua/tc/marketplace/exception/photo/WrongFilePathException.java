25da6cef09e6f8ccdf94b08537389b947c669816
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-07-17 11:14:05 +0300

    rename back 'filename' to 'path'

--- src/main/java/ua/tc/marketplace/exception/photo/WrongFilePathException.java
+++ src/main/java/ua/tc/marketplace/exception/photo/WrongFilePathException.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import ua.tc.marketplace.exception.model.CustomRuntimeException;
 
 /**
- * Exception thrown when there is an issue with a file filename.
+ * Exception thrown when there is an issue with a file path.
  *
  * <p>This exception is used to signal that an attempt to find or create a directory at the
- * specified filename has failed. It extends the {@link CustomRuntimeException} class and provides a
- * detailed error message including the problematic filename. The associated HTTP status for this
+ * specified path has failed. It extends the {@link CustomRuntimeException} class and provides a
+ * detailed error message including the problematic path. The associated HTTP status for this
  * exception is {@code HttpStatus.CONFLICT}.
  */
 public class WrongFilePathException extends CustomRuntimeException {
