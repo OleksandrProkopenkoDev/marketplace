bdfc75a35e279e3645948be6ff010a41fa804dbf
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-08-01 10:04:35 +0300

    default value for List<Ad> in userDto

--- src/main/java/ua/tc/marketplace/model/dto/user/UserDto.java
+++ src/main/java/ua/tc/marketplace/model/dto/user/UserDto.java
@@ -3,6 +3,7 @@ package ua.tc.marketplace.model.dto.user;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import ua.tc.marketplace.model.entity.Ad;
 import ua.tc.marketplace.model.entity.ContactInfo;
@@ -10,34 +11,23 @@ import ua.tc.marketplace.model.entity.Photo;
 
 public record UserDto(
     Long id,
-
-    @Schema(example = "taras@shevchenko.ua")
-    @NotBlank
-    String email,
-
+    @Schema(example = "taras@shevchenko.ua") @NotBlank String email,
     @Schema(example = "strong_secure_password_with_bigAndSmallLetters_and_digits_and_symbols")
-    @NotBlank
-    String password,
-
-    @Schema(example = "INDIVIDUAL")
-    @NotBlank
-    String userRole,
-
-    @Schema(example = "Taras")
-    @NotBlank
-    String firstName,
-
-    @Schema(example = "Shevchenko")
-    String lastName, // optional
-
+        @NotBlank
+        String password,
+    @Schema(example = "INDIVIDUAL") @NotBlank String userRole,
+    @Schema(example = "Taras") @NotBlank String firstName,
+    @Schema(example = "Shevchenko") String lastName, // optional
     Photo profilePicture,
-
     ContactInfo contactInfo,
-
     List<Ad> favorites,
-
     LocalDateTime createdAt,
-
     LocalDateTime updatedAt) {
 
+  public UserDto {
+    // Default values for nullable fields
+    if (favorites == null) {
+      favorites = new ArrayList<>();
+    }
+  }
 }
